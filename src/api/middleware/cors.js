6c829a1802569ec14b83015e0688b6fc0e9c3004
Alexandre Maeda <ale.b.maeda@gmail.com>
2021-09-21 16:46:59 -0300

    cors config

---
+++ src/api/middleware/cors.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const cors = require('cors');
+
+const whitelist = ['http://127.0.0.1:8080'];
+
+module.exports = cors({
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+});
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const handleErrors = require('./api/middleware/handleErrors');
 const routes = require('./routes');
+const cors = require('./api/middleware/cors');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors);
 app.use(routes);
 app.use(handleErrors);
 
