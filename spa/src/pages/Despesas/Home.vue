a67c69b80b7dddd1f2b2c99b9312c51a88032a31
Alexandre Maeda <ale.b.maeda@gmail.com>
2021-09-22 13:41:23 -0300

    Despesas

---
+++ spa/src/pages/Despesas/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <b-row>
+        <b-col cols="12">
+          <b-breadcrumb :items="breadCrumbItems"></b-breadcrumb>
+          <card>
+            <h2 slot="header" class="card-title mb-3">Despesas</h2>
+            <p>em breve</p>
+            <b-skeleton-table
+              :rows="5"
+              :columns="4"
+              :table-props="{ bordered: true, striped: true }"
+            ></b-skeleton-table>
+          </card>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      breadCrumbItems: [
+        {
+          text: 'Dashboard',
+          to: { name: 'Home' },
+        },
+        {
+          text: 'Despesas',
+          active: true,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
